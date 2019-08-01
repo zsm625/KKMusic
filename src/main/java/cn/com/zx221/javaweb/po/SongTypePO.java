c2c5d9d7f2da75eae6f460228d7bd8241049e20f
zl <2507006829@qq.com>
2019-08-01 17:06:42 +0800

    创建了1.SlsongListPO(歌曲歌单po)      2.SongListPO(歌单列表)      3.SongPO(歌单表)      4.SongType(歌单类型表)      5.UcollectCdPO(用户收藏专辑)      6.UcollectSingerPO(用户收藏歌手)      7.UcollectSonglistPO(用户收藏歌单)

---
+++ src/main/java/cn/com/zx221/javaweb/po/SongTypePO.java
@@ -0,0 +1,23 @@
+package cn.com.zx221.javaweb.po;
+
+public class SongTypePO {
+	private int songTypeId;
+	private String songTypeName;
+
+	public int getSongTypeId() {
+		return songTypeId;
+	}
+
+	public void setSongTypeId(int songTypeId) {
+		this.songTypeId = songTypeId;
+	}
+
+	public String getSongTypeName() {
+		return songTypeName;
+	}
+
+	public void setSongTypeName(String songTypeName) {
+		this.songTypeName = songTypeName;
+	}
+
+}
